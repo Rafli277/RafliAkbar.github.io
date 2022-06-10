a36d7dac006fd4c6163f45fd8b6a603d25204e98
RafliAkbar <rafliakbaraudiputra2@gmail.com>
2022-06-10 09:21:20 +0700

    some changes to make perfection

--- main.js
+++ main.js
@@ -57,19 +57,19 @@ const sr = ScrollReveal({
 // SCROLL HOME
 sr.reveal('.home__title', {});
 sr.reveal('.home__btn', { delay: 200 });
-sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__img', { delay: 200 });
 sr.reveal('.home__social-icon', { interval: 200 });
 // Scroll About
 sr.reveal('.about__img', {});
 sr.reveal('.about__subtitle', { delay: 200 });
-sr.reveal('.about__text', { delay: 400 });
+sr.reveal('.about__text', { delay: 200 });
 // scroll Skills
 sr.reveal('.skills__subtitle', {});
 sr.reveal('.skills__text', { delay: 200 });
 sr.reveal('.skills__data', { interval: 200 });
-sr.reveal('.skills__img', { delay: 400 });
+sr.reveal('.skills__img', { delay: 200 });
 // Scroll Works
 sr.reveal('.works__img', { interval: 200 });
 // Scroll Contact
-sr.reveal('.contact__input', { interval: 200 });
-sr.reveal('.contact__subtitle', { delay: 200 });
+sr.reveal('.contact__input', { interval: 100 });
+sr.reveal('.contact__subtitle', { delay: 50 });
